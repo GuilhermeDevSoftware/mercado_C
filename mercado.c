22d8a603653fc038fe5b0bfe7f17de5f279e2277
Guilherme Costa <guizeracostasilva@outlook.com>
2024-08-11 11:49:49 -0300

    feat functions

--- mercado.c
+++ mercado.c
@@ -14,13 +14,28 @@ typedef struct {
     int quantidade;
 } Carrinho;
 
+void infoProduto(Produto prod);
+void menu();
+void cadastrarProduto();
+void listarProduto();
+void visualizarCarrinho();
+Produto pegarProdutoPorCodigo(int codigo);
+int * temNoCarrinho(int codigo);
+void fecharPedido();
 
+static int contador_produto = 0;
+static int contador_carrinho = 0;
+static Carrinho carrinho[50];
+static Produto produto[50];
 
 
 
 int main(){
 
-
-
+    menu();
     return 0;
+}
+
+void infoProduto(Produto prod){
+    
 }
