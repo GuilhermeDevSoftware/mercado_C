47ec1a04bbe8836f98f48e1dd4fb8d7d2be8d978
Guilherme Costa <guizeracostasilva@outlook.com>
2024-08-11 11:42:11 -0300

    Creat project C, system trade

---
+++ mercado.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <windows.h>
+
+typedef struct {
+    int codigo;
+    char nome[30];
+    float preco;
+} Produto;
+
+typedef struct {
+    Produto produto;
+    int quantidade;
+} Carrinho;
+
+
+
+
+
+int main(){
+
+
+
+    return 0;
+}
